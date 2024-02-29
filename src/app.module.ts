1d023f3db8920e2cea6b4f2a648ef0b1aa50a492
ehdtjs0612 <inko51366@naver.com>
2024-02-29 13:34:55 +0900

    feat: connect database in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ArticleModule } from './api/article/article.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ArticleModule,
+    DatabaseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        host: configService.get('POSTGRESQL_HOST') as string,
+        port: configService.get('POSTGRESQL_PORT') as number,
+        user: configService.get('POSTGRESQL_USER') as string,
+        password: configService.get('POSTGRESQL_PASSWORD') as string,
+        database: configService.get('POSTGRESQL_DATABASE') as string,
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
